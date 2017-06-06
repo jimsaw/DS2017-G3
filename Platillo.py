cfa3e5f8d2eb94e13d3c05e1f70eac089f153039
Unknown <sard1996@hotmail.com>
2017-06-06 16:29:18 -0500

    Prueba

--- Platillo.py
+++ Platillo.py
@@ -16,3 +16,5 @@ class Platillo:
 
     def __str__(self):
         return 'Nombre del platillo: %s \nRestaurante: %s \nCategoria: %s \nIngredientes: %s \nDescripcion: %s' % (self.nombre, self.restaurante, self.categoria, self.ingredientes, self.descripcion)
+
+    dasdas
